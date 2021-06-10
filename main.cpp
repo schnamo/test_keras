6959ed9a0a8e3667aaf1b165f64bff484c57592c
schnamo <ch.tumescheit@gmail.com>
2021-06-10 14:50:40 +0900

    user fix

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main()  {
 
     int seqLen = 17;
-    char seq[] = "GDGIYEAHDFTYY\0";
+    //char seq[] = "GDGIYEAHDFTYY\0";
     char seq2[18]  = {'A', 'G', 'M', 'C', 'E', 'I', 'S', 'D', 'A', 'T', 'F', 'E', 'V', 'S', 'D', 'E', 'I','\0'};
     char *seq_pointer = seq2;
 
